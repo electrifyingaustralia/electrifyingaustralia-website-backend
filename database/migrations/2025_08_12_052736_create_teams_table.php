dd7cbdfbc2cf22553cfbf893436b3a1d9a2a12e9
Md Estyaque Hossain <powermanfahim@gmail.com>
2025-08-12 12:35:52 +0600

    Admin and Team migrations file added

---
+++ database/migrations/2025_08_12_052736_create_teams_table.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('teams', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name');
+
+            $table->string('email');
+
+            $table->string('phone')->nullable();
+
+            $table->string('designation')->nullable();
+
+            $table->string('avatar')->nullable();
+
+            $table->string('title')->nullable();
+
+            $table->longText('description')->nullable();
+
+            $table->text('website')->nullable();
+
+            $table->boolean('is_active')->default(true);
+
+            $table->string('twitter_link')->nullable();
+
+            $table->string('instagram_link')->nullable();
+
+            $table->string('facebook_link')->nullable();
+
+            $table->string('pinterest_link')->nullable();
+
+            $table->string('youtube_link')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('teams');
+    }
+};
