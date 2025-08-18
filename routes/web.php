d0b2add8c1d9d0e15d517c58256e430d28bc58cd
sojibpml <it.powermanlocal02@gmail.com>
2025-08-18 08:22:18 +0600

    Header, footer and sidebar added

--- routes/web.php
+++ routes/web.php
@@ -4,18 +4,18 @@ use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', action: function () {
-    return view('dashbord');
+    return view('backend.dashboard.dashboard');
 });
 
 
 Route::get('/users',function(){
-    return view('Backend.layouts.admin.index');
+    return view('Backend.admin.index');
 });
 
 
-// Route::get('/login',function(){
-//     return view('backend.layouts.auth.login.login');
-// });
+Route::get('/login',function(){
+    return view('backend.layouts.auth.login.login');
+});
 
 // Route::get('/register',function(){
 //     return view('backend.layouts.auth.register.register');
