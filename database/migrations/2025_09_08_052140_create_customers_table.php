a399415a0ccf9417019ed3ca016a8b46c8ca6838
Md Estyaque Hossain <powermanfahim@gmail.com>
2025-09-26 08:53:13 +0600

    merge with sojib branch

--- database/migrations/2025_09_08_052140_create_customers_table.php
+++ database/migrations/2025_09_08_052140_create_customers_table.php
@@ -13,12 +13,12 @@ return new class extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('email');
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('email')->nullable();
             $table->string('phone')->nullable();
             $table->string('address')->nullable();
-            $table->string('status')->default('pending');
+            // $table->string('status')->default('pending');
             $table->timestamps();
         });
     }
