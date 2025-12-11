544d4039bb4080c0e763582ae446b53e158be919
electrifyingaustralia <sourovpal.ea@gmail.com>
2025-12-11 15:01:02 +0600

    Deploy

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', 'https://api.electrifyingaustralia.com.au,https://www.electrifyingaustralia.com.au')),
+    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', 'https://api.electrifyingaustralia.com.au,https://www.electrifyingaustralia.com.au,https://electrifyingaustralia.com.au')),
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
